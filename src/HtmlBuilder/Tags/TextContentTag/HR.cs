d628481a703b5baa3e41db8e600d986acb0e8fcc
daniyal2224 <daniyal2224@gmail.com>
2025-05-13 22:03:26 +0330

    Fix

--- src/HtmlBuilder/Tags/TextContentTag/HR.cs
+++ src/HtmlBuilder/Tags/TextContentTag/HR.cs
@@ -1,2 +1,8 @@
 namespace HtmlBuilder.Tags.TextContentTag;
-public class Hr() : SingleTag("hr");
+public class Hr() : SingleTag("hr")
+{
+    public override void Accept(IHtmlVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
